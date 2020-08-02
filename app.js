3efea9e280f2babde0405363567a14d85c50ec75
profaddy <adnansaify11@gmail.com>
2020-08-02 21:52:44 +0530

    node modules removed

--- app.js
+++ app.js
@@ -39,7 +39,15 @@ app.use((res, req, next) => {
   error.status = 404;
   next(error);
 });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
 // app.use((error, res, req, next) => {
 //     // res.status = error.status || 500;
 //      res.status(error.status).json({
