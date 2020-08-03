8c332e46c345b3a8969af214b4e46852cc898479
profaddy <adnansaify11@gmail.com>
2020-08-03 08:36:33 +0530

    node modules removed and api tweaked

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 // IMPORT MODELS
 require('./models/Product');
+const productRoutes = require("./api/routes/products");
+const userRoutes = require("./api/routes/users");
+const entriesRoutes = require("./api/routes/entries");
 
 const app = express();
 
@@ -14,9 +17,31 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://adnansaify11:oC79bIbG
 
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE");
+    res.status(200).json({})
+  }
+  next();
+})
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/entries", entriesRoutes);
+
 
 //IMPORT ROUTES
 require('./routes/productRoutes')(app);
+// require('./api/routes/productRoutes')(app);
+// require('./api//routes/users')(app);
+// require('./api/routes/entries')(app);
+app.use((res, req, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
